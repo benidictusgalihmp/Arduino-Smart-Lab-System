2fdd01dd2894e4cf1e573d1a50ed2cdca6147481
benidictusgalihmp <68523239+benidictusgalihmp@users.noreply.github.com>
2022-04-24 13:12:46 +0700

    style: change space size

--- arduino1.cpp
+++ arduino1.cpp
@@ -80,162 +80,163 @@ unsigned long threshold;
 
 void setup()
 {
-  // setup lcd resolution rows x columns (col, row)
-  // this resolution is based on the lcd itself
-  // in our case, it has 2 rows x 16 columns
-  lcd.begin(16, 2);
+    // setup lcd resolution rows x columns (col, row)
+    // this resolution is based on the lcd itself
+    // in our case, it has 2 rows x 16 columns
+    lcd.begin(16, 2);
 
-  // setup the serial
-  Serial.begin(9600);
+    // setup the serial
+    Serial.begin(9600);
 
-  servo_10.attach(10);
-  servo_10.write(0);
+    servo_10.attach(10);
+    servo_10.write(0);
 
-  // setup the I2C communication
-  Wire.begin();
+    // setup the I2C communication
+    Wire.begin();
 }
 
 void loop()
 {
-  /* --- SETUP PER SECOND BOOLEAN --- */
-  currentMillis = millis();
-  threshold = startMillis + period;
+    /* --- SETUP PER SECOND BOOLEAN --- */
+    currentMillis = millis();
+    threshold = startMillis + period;
 
-  if (currentMillis >= threshold)
-  {
-    startMillis = currentMillis;
-    isPerSecond = true;
-  }
-  else
-  {
-    isPerSecond = false;
-  }
-
-  if (!isPower && !isTimeUp)
-  {
-    // get the value char key from keypad
-    char keypressed = powerKeypad.getKey();
-
-    if (keypressed != NO_KEY &&
-        keypressed == 'A')
+    if (currentMillis >= threshold)
     {
-      isPower = true;
+        startMillis = currentMillis;
+        isPerSecond = true;
     }
-  }
-  else if (isPower && !isTimeUp)
-  {
-    // get the value char key from keypad
-    char keypressed = inputKeypad.getKey();
-
-    // reset the lcd screen
-    if (!isClearScreen)
+    else
     {
-      lcd.clear();
-      isClearScreen = true;
+        isPerSecond = false;
     }
 
-    /* --- SETUP PRINT PASSWORD --- */
-    // set the start cursor position (col, row)
-    lcd.setCursor(0, 0);
-
-    // write string into lcd
-    lcd.print("PASSWORD: ");
-    lcd.print(keypadEntry);
-
-    /* --- SETUP PRINT COUNTDOWN --- */
-    if (isPerSecond)
+    if (!isPower && !isTimeUp)
     {
-      entryCountdown -= 1;
+        // get the value char key from keypad
+        char keypressed = powerKeypad.getKey();
+
+        if (keypressed != NO_KEY &&
+            keypressed == 'A')
+        {
+            isPower = true;
+        }
     }
-
-    lcd.setCursor(0, 1);
-
-    String countdownText = (entryCountdown < 10) ? "COUNTDOWN: 0" : "COUNTDOWN: ";
-    lcd.print(countdownText);
-
-    lcd.print(entryCountdown);
-
-    isTimeUp = (entryCountdown <= 0) ? true : false;
-
-    /* --- ENTRY PASSWORD HANDLING --- */
-    if (keypressed != NO_KEY &&
-        keypadEntry.length() < 4)
+    else if (isPower && !isTimeUp)
     {
-      keypadEntry += keypressed;
+        // get the value char key from keypad
+        char keypressed = inputKeypad.getKey();
+
+        // reset the lcd screen
+        if (!isClearScreen)
+        {
+            lcd.clear();
+            isClearScreen = true;
+        }
+
+        /* --- SETUP PRINT PASSWORD --- */
+        // set the start cursor position (col, row)
+        lcd.setCursor(0, 0);
+
+        // write string into lcd
+        lcd.print("PASSWORD: ");
+        lcd.print(keypadEntry);
+
+        /* --- SETUP PRINT COUNTDOWN --- */
+        if (isPerSecond)
+        {
+            entryCountdown -= 1;
+        }
+
+        lcd.setCursor(0, 1);
+
+        String countdownText = (entryCountdown < 10) ? "COUNTDOWN: 0" : "COUNTDOWN: ";
+        lcd.print(countdownText);
+
+        lcd.print(entryCountdown);
+
+        isTimeUp = (entryCountdown <= 0) ? true : false;
+
+        /* --- ENTRY PASSWORD HANDLING --- */
+        if (keypressed != NO_KEY &&
+            keypadEntry.length() < 4)
+        {
+            keypadEntry += keypressed;
+        }
+        else if (keypadEntry.length() == 4)
+        {
+            // clear output screen and set default cursor position
+            lcd.clear();
+
+            // entry password is correct
+            if (keypadEntry == password)
+            {
+                servo_10.write(openDoor);
+
+                lcd.setCursor(0, 0);
+                lcd.print("UNLOCKED");
+                lcd.setCursor(0, 1);
+                lcd.print("WELCOME MOBITA");
+
+                // create transmission with address 8
+                Wire.beginTransmission(8);
+                Wire.write("enteropen");
+                Wire.endTransmission();
+
+                entryCountdown = 15;
+                isClearScreen = false;
+                delay(1000);
+            }
+            else
+            {
+                lcd.setCursor(0, 0);
+                lcd.print("INCORRECT");
+                lcd.setCursor(0, 1);
+                lcd.print("PASSWORD");
+                delay(1000);
+            }
+            keypadEntry = "";
+        }
+
+        /* --- RESET STATE --- */
+        if (entryCountdown <= 0)
+        {
+            entryCountdown = 15;
+            isClearScreen = false;
+            isTimeUp = true;
+            delay(500);
+        }
     }
-    else if (keypadEntry.length() == 4)
+    else if (isTimeUp)
     {
-      // clear output screen and set default cursor position
-      lcd.clear();
+        // reset the lcd screen
+        if (!isClearScreen)
+        {
+            lcd.clear();
+            isClearScreen = true;
+        }
 
-      // entry password is correct
-      if (keypadEntry == password)
-      {
-        servo_10.write(openDoor);
+        if (isPerSecond)
+        {
+            entryCooldown -= 1;
+        }
 
         lcd.setCursor(0, 0);
-        lcd.print("UNLOCKED");
+        lcd.print("TIME IS UP");
         lcd.setCursor(0, 1);
-        lcd.print("WELCOME MOBITA");
+        String cooldownText = (entryCooldown < 10) ? "WAIT  " : "WAIT ";
+        lcd.print(cooldownText);
+        lcd.print(entryCooldown);
+        lcd.print(" SECONDS");
 
-        // create transmission with address 8
-        Wire.beginTransmission(8);
-        Wire.write("enteropen");
-        Wire.endTransmission();
-
-        entryCountdown = 15;
-        isClearScreen = false;
-        delay(1000);
-      }
-      else
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("INCORRECT");
-        lcd.setCursor(0, 1);
-        lcd.print("PASSWORD");
-        delay(1000);
-      }
-      keypadEntry = "";
+        /* --- RESET STATE --- */
+        if (entryCooldown <= 0)
+        {
+            entryCooldown = 10;
+            isClearScreen = false;
+            isTimeUp = false;
+            keypadEntry = "";
+            delay(500);
+        }
     }
-    /* --- RESET STATE --- */
-    if (entryCountdown <= 0)
-    {
-      entryCountdown = 15;
-      isClearScreen = false;
-      isTimeUp = true;
-      delay(500);
-    }
-  }
-  else if (isTimeUp)
-  {
-    // reset the lcd screen
-    if (!isClearScreen)
-    {
-      lcd.clear();
-      isClearScreen = true;
-    }
-
-    if (isPerSecond)
-    {
-      entryCooldown -= 1;
-    }
-
-    lcd.setCursor(0, 0);
-    lcd.print("TIME IS UP");
-    lcd.setCursor(0, 1);
-    String cooldownText = (entryCooldown < 10) ? "WAIT  " : "WAIT ";
-    lcd.print(cooldownText);
-    lcd.print(entryCooldown);
-    lcd.print(" SECONDS");
-
-    /* --- RESET STATE --- */
-    if (entryCooldown <= 0)
-    {
-      entryCooldown = 10;
-      isClearScreen = false;
-      isTimeUp = false;
-      keypadEntry = "";
-      delay(500);
-    }
-  }
 }
